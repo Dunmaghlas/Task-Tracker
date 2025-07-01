9fea50d3a6d3f30a6772b006bbf4568665974b4f
Artem <gorfard@yandex.ru>
2025-07-01 17:08:52 +0700

    index.ts draft was made, fixed some conf moments in package.json + tsconfig.json

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,37 @@
-"use strict";
-// src/index.ts
-const greeting = "Hello, TypeScript!";
-console.log(greeting);
-module.exports = {};
+class TaskTracker {
+    constructor() {
+        this.commands = {};
+        this.first_arg = process.argv.slice(2);
+        this.second_arg = process.argv.slice(3);
+        this.third_arg = process.argv.slice(4);
+        this.commands = {};
+        this.setCommands();
+        this.Execute();
+    }
+    setCommands() {
+        this.commands = {
+            add: this.addTask,
+            remove: this.removeTask,
+            help: this.helpTask,
+        };
+    }
+    addTask() {
+    }
+    removeTask() {
+    }
+    helpTask() {
+    }
+    Execute() {
+        if (this.first_arg.length === 0 || this.second_arg.length === 0 || this.first_arg[0] === "help") {
+            console.log(`
+                Usage: task-traker [command] [id] [data]
+                
+                Commands:
+                    help
+                    add
+                    remove
+                `);
+        }
+    }
+}
+new TaskTracker();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-// src/index.ts
-const greeting: string = "Hello, TypeScript!";
-console.log(greeting);
+class TaskTracker{
+    first_arg:  string[];
+    second_arg: string[];
+    third_arg:  string[];
+    commands:   Record<string, Function> = {};
 
-export = {} 
+    constructor(){
+        this.first_arg  =   process.argv.slice(2);
+        this.second_arg =   process.argv.slice(3);
+        this.third_arg  =   process.argv.slice(4);
+        this.commands   =   {};
+
+        this.setCommands();
+        this.Execute();
+    }
+
+    private setCommands(){
+        this.commands = {
+            add:    this.addTask,
+            remove: this.removeTask,
+            help:   this.helpTask,
+        }
+    }
+
+    private addTask(){
+
+    }
+
+    private removeTask(){
+
+    }
+    
+    private helpTask(){
+
+    }
+
+    private Execute(){
+        if (this.first_arg.length === 0 || this.second_arg.length === 0 || this.first_arg[0] === "help") {
+            console.log(`
+                Usage: task-traker [command] [id] [data]
+                
+                Commands:
+                    help
+                    add
+                    remove
+                `);
+        }
+    }
+}
+
+new TaskTracker();
